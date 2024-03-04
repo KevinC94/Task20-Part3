d49bd450352cbcc82efd81cb96a6bc4996f061f2
KevinC94 <kevincrosby94@gmail.com>
2024-03-04 14:40:22 +0000

    added comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
+# asks for user input for information to print
 print_function = input("Please input what you need to print: ")
 print (print_function)
 
+#asks user for name and prints
 name = input("please enter your name: ")
 print (name)
